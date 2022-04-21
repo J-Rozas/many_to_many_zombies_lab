7d40a1dca559adef13132cb6e13dfff11621e4e7
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-21 14:46:16 +0100

    Add route to /bitings

--- controllers/bitings_controller.py
+++ controllers/bitings_controller.py
@@ -1,8 +1,19 @@
 from flask import Blueprint, Flask, redirect, render_template, request
 
+# import the required model
+from models.biting import Biting
+
+# import the required repository
+from repositories import biting_repository
+
 bitings_blueprint = Blueprint("bitings", __name__)
 
 # INDEX
+@bitings_blueprint.route("/bitings")
+def bitings():
+    bitings = biting_repository.select_all()
+    return render_template("bitings/index.html", bitings = bitings)
+
 
 # NEW
 
